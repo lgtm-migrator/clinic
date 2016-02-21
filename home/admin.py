c5b42ccb4fc81b60700a2ee0894f12c0ff614aaa
Van-Duyet Le <lvduit08@gmail.com>
2016-02-21 16:59:22 +0700

    Default working day admin

--- home/admin.py
+++ home/admin.py
@@ -4,9 +4,12 @@ from django.contrib import admin
 from django.conf.urls import url
 from django.template.response import TemplateResponse
 from django.forms.models import BaseModelFormSet
-from .models import Store, WorkingDay, HolidayWorking, Region, NearestStation, Sortkey, Holiday
+from django.db import models
+
+from .models import *
 
 class WorkingDayInlineAdminForm(forms.ModelForm):
+	type = models.CharField(default="Mo")
 	class Meta:
 		model = WorkingDay
 		exclude = []
@@ -25,9 +28,20 @@ class WorkingDayInlineFormSet(BaseModelFormSet):
 
 class WorkingDayInline(admin.TabularInline):
 	model = WorkingDay
-	extra = 8
 	form = WorkingDayInlineAdminForm
-	# formset = WorkingDayInlineFormSet
+	def get_extra(self, request, obj=None, **kwargs):
+		extra = 8
+		if obj:
+			return 0
+		return extra
+	def get_formset(self, request, obj=None, **kwargs):
+		initial = [ { 'type': d[0:2] for d in x } for x in WORKING_DAY ]
+		print (initial)
+		if request.method != "GET":
+			initial = []
+		formset = super(WorkingDayInline, self).get_formset(request, obj, **kwargs)
+		formset.__init__ = curry(formset.__init__, initial=initial)
+		return formset
 	
 class HolidayWorkingInline(admin.TabularInline):
 	model = HolidayWorking
@@ -39,8 +53,7 @@ class StoreAmin(admin.ModelAdmin):
 	list_display = ('store_id', 'name', 'phone', 'mail', 'access')
 	list_filter = ('store_id', 'name', 'phone')
 	fieldsets = [
-		(None, { 'fields': [ 'display', 'store_id', 'name', 'image', 'comment', 'phone', 'mail', 'access'], 'classes': ('wide', ) }),
-		(None, { 'fields': [ 'region', 'nearest_station'], 'classes': ('wide', ) }),
+		(None, { 'fields': [ 'display', 'store_id', 'name', 'image', 'comment', 'region', 'nearest_station', 'phone', 'mail', 'access'], 'classes': ('wide', ) }),
 	]
 	inlines = [ WorkingDayInline, HolidayWorkingInline, ]
 	prepopulated_fields = { 'name': ['name'] }
@@ -50,6 +63,11 @@ class StoreAmin(admin.ModelAdmin):
 		return '<img src="/%s" />' % instance.image
 	image_show.allow_tags = True
 	image_show.short_description = 'Image'
+
+	class Meta:
+		from django.conf import settings
+		media_url = getattr(settings, 'MEDIA_URL', '/media')
+		js = [ media_url+'/admin/long-lat-render.js', ]
 
 class HolidayAdmin(admin.ModelAdmin):
 	pass
