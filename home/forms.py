f6efb57412e3ff06ca171704586365c33e5d5fd3
tiennv91 <tiennv.study@gmail.com>
2016-02-23 00:05:34 +0700

    change invalid email

--- home/forms.py
+++ home/forms.py
@@ -18,5 +18,5 @@ class ScheForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(ScheForm, self).__init__(*args, **kwargs)
         self.fields['name'].error_messages = {'required': '氏名は必須です'}
-        self.fields['email'].error_messages = {'required': 'メールアドレスは必須です'}
+        self.fields['email'].error_messages = {'required': 'メールアドレスは必須です', 'invalid':"メールアドレスの形式が正しくありません"}
         self.fields['phone'].error_messages = {'required': '電話番号は必須です'}
