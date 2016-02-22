9b0464a77a13ad5d0a2f7ba92c9176caecda7e2e
tiennv91 <tiennv.study@gmail.com>
2016-02-22 14:06:51 +0700

    fix required fields

--- home/forms.py
+++ home/forms.py
@@ -9,8 +9,14 @@ class ScheForm(forms.ModelForm):
         model = Schedule
         fields = ('name', 'phone', 'email', 'symptom')
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'氏名', 'required': True}),
-            'email': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'電話番号', 'required': True}),
-            'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'連絡先', 'required': True}),
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'氏名'}),
+            'email': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'連絡先'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'電話番号'}),
             'symptom': forms.Textarea(attrs={'class': 'form-control', 'rows':'5', 'placeholder':'症状などをご入力ください'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super(ScheForm, self).__init__(*args, **kwargs)
+        self.fields['name'].error_messages = {'required': '氏名は必須です'}
+        self.fields['email'].error_messages = {'required': '連絡先は必須です'}
+        self.fields['phone'].error_messages = {'required': '電話番号は必須です'}
